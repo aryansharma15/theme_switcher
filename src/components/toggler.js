f82ba34efac22861c8293c10c201031d545abdb4
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-19 18:20:17 +0530

    cleaned and checking struct

---
+++ src/components/toggler.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Toggler = () => {
+	return (
+		<div>
+			<button>Toggle Theme</button>
+			<input type="checkbox" />
+		</div>
+	);
+};
+
+export default Toggler;
