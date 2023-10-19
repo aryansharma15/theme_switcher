941c15dd384c573eee8275192e8dfc155759130a
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-10-19 23:17:11 +0530

    daybreak

--- src/components/toggler.js
+++ src/components/toggler.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import "../App.css";
 
-const Toggler = () => {
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+
+const Toggler = (props) => {
+	const [state, setState] = useState(true);
+
 	return (
-		<div>
-			<button>Toggle Theme</button>
-			<input type="checkbox" />
+		<div className="my-5">
+			{/* <button className="mx-3">Toggle Theme</button>
+			<Button className="mx-3" color="danger">
+				Toggle theme
+			</Button> */}
+			<Form className="container">
+				<FormGroup switch className="centered">
+					<Input type="switch" role="switch" className="toggler-input" />
+					<Label check className="">
+						Toggle
+					</Label>
+				</FormGroup>
+			</Form>
+			{/* <Label className="custom-toggle custom-toggle-primary">
+				<Input type="checkbox" checked="" className="mx-3" />
+				<span className="custom-toggle-slider rounded-circle" data-label-off="OFF" data-label-on="ON"></span>
+			</Label> */}
 		</div>
 	);
 };
