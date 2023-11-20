bd0d8660d3fd4bee5c4bb1c22a965d097870ce9d
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-11-20 18:18:09 +0530

    mousepress error

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,27 +2,60 @@ import React, { useState } from "react";
 import "../App.css";
 
 function Counter() {
-	let [count, setCount] = useState("0");
+	const [count, setCount] = useState(0);
+	const [intervalId, setIntervalId] = useState(null);
+
+	const handleIncrement = () => {
+		setCount(count + 1);
+	};
+
+	const handleDecrement = () => {
+		setCount(count - 1);
+	};
+
+	const startInc = () => {
+		handleIncrement();
+
+		const id = setInterval(handleIncrement, 200);
+		setIntervalId(id);
+	};
+
+	const startDec = () => {
+		handleDecrement();
+
+		const id = setInterval(handleDecrement, 200);
+		setIntervalId(id);
+	};
+
+	const stopTheCount = () => {
+		// clearing the interval when button is no longer pressed
+		if (intervalId) {
+			clearInterval(intervalId);
+			setIntervalId(null);
+		}
+	};
 
 	return (
 		<div className="counter">
-			<h1>Currently: {count}</h1>
-			<button
-				className="counter-btns incr-btn"
-				onClick={() => {
-					setCount((count += 1));
-				}}
-			>
-				Increment
-			</button>
-			<button
-				className="counter-btns decr-btn"
-				onClick={() => {
-					setCount((count -= 1));
-				}}
-			>
-				Decrement
-			</button>
+			<div className="counter-content">
+				<h1>Current count: {count}</h1>
+				<div className="btns">
+					<button className="counter-btns incr-btn" onMouseDown={startInc} onMouseUp={stopTheCount}>
+						Increment
+					</button>
+					<button className="counter-btns decr-btn" onMouseDown={startDec} onMouseUp={stopTheCount}>
+						Decrement
+					</button>
+					<button
+						className="counter-btns reset-btn"
+						onClick={() => {
+							setCount(0);
+						}}
+					>
+						Reset
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
